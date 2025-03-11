6428aba4391506f4adee75a54967923617be88d5
yuki-k1313 <yuki1313@naver.com>
2025-03-11 12:37:27 +0900

    2025-03-11

--- src/layouts/Layout/index.tsx
+++ src/layouts/Layout/index.tsx
@@ -1,12 +1,70 @@
-import React from 'react'
-import { Outlet } from 'react-router'
+import React, { useEffect, useRef, useState } from 'react'
+import { Outlet, useLocation } from 'react-router'
 
 import './style.css';
 
+// component: 공통 레이아웃 컴포넌트 //
 export default function Layout() {
+
+    // state: 경로 상태 //
+    const { pathname } = useLocation();
+
+    // state: My Content List 요소 참조 //
+    const MyContentListRef = useRef<HTMLDivElement | null>(null);
+
+    // state: My Content 드롭다운 상태 //
+    const [showMyContent, setShowMyContent] = useState<boolean>(false);
+
+    // variable: 기억력 검사 클래스 //
+    const memoryTestClass = pathname.startsWith('/memory-test') ? 'navigation-item active' : 'navigation-item';
+    // variable: 집중력 검사 클래스 //
+    const concentrationTestClass = pathname.startsWith('/concentration-test') ? 'navigation-item active' : 'navigation-item';
+
+    // event handler: My Content 클릭 이벤트 처리 //
+    const onMyContentClickHandler = () => {
+        setShowMyContent(!showMyContent);
+    }
+
+    // effect: My Content 드롭다운 상태가 변경될시 실행할 함수 //
+    useEffect(() => {
+        const onOutsideClickHandler = (event: any) => {
+            if (
+                MyContentListRef.current && 
+                !MyContentListRef.current.contains(event.target as Node)
+            ) {
+                setShowMyContent(false);
+            }
+        };
+
+        if (!showMyContent) return;
+
+        document.addEventListener('mousedown', onOutsideClickHandler);
+
+    }, [showMyContent]);
+
+    // render: 공통 레이아웃 컴포넌트 렌더링 //
     return (
-        <div>
-            <Outlet />
+        <div id='layout-wrapper'>
+            <div id='top-bar'>
+                <div className='navigation'>
+                    <div className='title'>Memories</div>
+                    <div className='navigation-list'>
+                        <div className={memoryTestClass}>기억력 검사</div>
+                        <div className={concentrationTestClass}>집중력 검사</div>
+                    </div>
+                </div>
+                <div className='my-content' onClick={onMyContentClickHandler}>
+                    {showMyContent &&
+                    <div ref={MyContentListRef} className='my-content-list'>
+                        <div className='my-content-item'>일기</div>
+                        <div className='my-content-item'>로그아웃</div>
+                    </div>
+                    }   
+                </div>
+            </div>
+            <div id='main'>
+                <Outlet />
+            </div>
         </div>
     )
 }
--- src/views/Auth/SignUp/index.tsx
+++ src/views/Auth/SignUp/index.tsx
@@ -1,7 +1,11 @@
-import React, { ChangeEvent, useState } from 'react';
-import './style.css';
+import React, { ChangeEvent, useEffect, useState } from 'react';
+import { Address, useDaumPostcodePopup } from 'react-daum-postcode';
 import { AuthPage } from 'src/types/aliases';
+
+import './style.css';
+
 import InputBox from 'src/components/InputBox';
+
 
 // interface: 회원가입 컴포넌트 속성 //
 interface Props {
@@ -40,28 +44,70 @@ export default function SignUp(props: Props) {
     // state: 사용자 아이디 메세지 에러 상태 //
     const [userIdMessageError, setUserIdMessageError] = useState<boolean>(false);
 
+    // state: 사용자 아이디 중복 확인 상태 //
+    const [isUserIdChecked, setUserIdChecked] = useState<boolean>(false);
+    // state: 사용자 비밀번호 패턴 일치 상태 //
+    const [isUserPasswordChecked, setUserPasswordChecked] = useState<boolean>(false);
+    // state: 사용자 비밀번호 동일 여부 상태 //
+    const [isUserPasswordEqual, setUserPasswordEqual] = useState<boolean>(false);
+    
+
+    // variable: 중복 확인 버튼 활성화 //
+    const isUserIdCheckButtonActive = userId !== '';
+    // variable: 회원가입 버튼 활성화 //
+    const isSignUpButtonActive = userName && userId && userPassword && userPasswordCheck && userAddress && 
+    isUserIdChecked && isUserPasswordChecked && isUserPasswordEqual;
+    // variable: 회원가입 버튼 클래스 //
+    const signUpButtonClass = `button ${isSignUpButtonActive ? 'primary' : 'disable'} fullwidth`;
+
+    // function: 다음 포스트 코드 팝업 오픈 함수 //
+    const open = useDaumPostcodePopup();
+
+    // function: 다음 포스트 코드 완료 처리 함수 //
+    const daumPostCompletHandler = (data: Address) => {
+        const { address } = data;
+        setUserAddress(address);
+        setUserAddressMessage('');
+    };
+
     // event handler: 사용자 이름 변경 이벤트 처리 //
     const onUserNameChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
         const { value } = event.target;
         setUserName(value);
+
+        setUserNameMessage('');
     };
 
     // event handler: 사용자 아이디 변경 이벤트 처리 //
     const onUserIdChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
         const { value } = event.target;
         setUserId(value);
+
+        setUserIdChecked(false);
+        setUserIdMessage('');
+        setUserIdMessageError(false);
     };
 
     // event handler: 사용자 비밀번호 변경 이벤트 처리 //
     const onUserPasswordChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
         const { value } = event.target;
         setUserPassword(value);
+
+        const regexp = /^(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9]{8,13}$/;
+        const isMatch = regexp.test(value);
+        const message = isMatch ? '' : '영문, 숫자를 혼용하여 8 ~ 13자 입력해주세요';
+        setUserPasswordMessage(message);
+        setUserPasswordChecked(isMatch);
     };
 
     // event handler: 사용자 비밀번호 확인 변경 이벤트 처리 //
     const onUserPasswordCheckChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
         const { value } = event.target;
         setUserPasswordCheck(value);
+
+        const isMatch = value === userPassword;
+        const message = isMatch ? '' : '비밀번호가 일치하지 않습니다.'
+        setUserPasswordCheckMessage(message);
     };
 
     // event handler: 사용자 주소 변경 이벤트 처리 //
@@ -78,16 +124,42 @@ export default function SignUp(props: Props) {
 
     // event handler: 중복 확인 버튼 클릭 이벤트 처리 //
     const onCheckUserIdClickHandler = () => {
+        if(!isUserIdCheckButtonActive) return;
 
+        const idList = ['qwer1234', 'rewq4321', 'poiu0987'];
+        const isExist = idList.includes(userId);
+        const message = isExist ? '이미 사용중인 아이디 입니다.' : '사용 가능한 아이디 입니다.';
+        setUserIdMessage(message);
+        setUserIdMessageError(isExist);
+        setUserIdChecked(!isExist);
     };
 
     // event handler: 주소 검색 버튼 클릭 이벤트 처리 //
     const onSearchAddressClickHandler = () => {
-
+        open({ onComplete: daumPostCompletHandler });
     };
 
-    // variable: 중복 확인 버튼 활성화 //
-    const isUserIdCheckButtonActive = userId !== '';
+    // event handler: 회원가입 버튼 클릭 이벤트 처리 //
+    const onSingUpClickHandler = () => {
+        if(!userName) setUserNameMessage('이름을 입력해주세요.');
+        if(!userPassword) setUserPasswordMessage('비밀번호를 입력해주세요.')
+        if(!userAddress) setUserAddressMessage('주소를 입력해주세요.');
+        if(!isUserIdChecked) {
+            setUserIdMessage('아이디 중복 확인해주세요.')
+            setUserIdMessageError(true);
+        } 
+        if(!isSignUpButtonActive) return;
+
+        alert('회원가입!');
+    };
+
+    // effect: 사용자 비밀번호 또는 사용자 비밀번호 확인이 변경될시 실행할 함수 //
+    useEffect(() => {
+        const isMatch = userPasswordCheck === userPassword;
+        const message = isMatch ? '' : '비밀번호가 일치하지 않습니다.'
+        setUserPasswordCheckMessage(message);
+        setUserPasswordEqual(isMatch);
+    }, [userPassword, userPasswordCheck])
 
     // render: 회원가입 컴포넌트 렌더링 //
     return (
@@ -117,7 +189,7 @@ export default function SignUp(props: Props) {
 
             </div>
             <div className='button-container'>
-                <div className='button disable fullwidth'>회원가입</div>
+                <div className={signUpButtonClass} onClick={onSingUpClickHandler}>회원가입</div>
                 <div className='link' onClick={() => onpageChange('sign-in')}>로그인</div>
             </div>
         </div>
