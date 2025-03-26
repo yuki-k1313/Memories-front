3c53deed3a7f9fc347c9c6dd053a69d9849cb1d4
yuki-k1313 <yuki1313@naver.com>
2025-03-26 16:59:34 +0900

    2025-03-24

--- src/types/interfaces/index.ts
+++ src/types/interfaces/index.ts
@@ -3,7 +3,7 @@ import MemoryCard from './memory-card.interface';
 import MemoryTest from './memory-test.interface';
 import ConcentrationTest from './concentration-test.interface';
 
-export type { 
+export type {
   Diary,
   MemoryCard,
   MemoryTest,
