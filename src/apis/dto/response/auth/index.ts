317a4599cdc8d4c2c879e67661b2beb542915490
yuki-k1313 <yuki1313@naver.com>
2025-03-26 13:35:45 +0900

    2025-03-17

--- src/apis/dto/response/auth/index.ts
+++ src/apis/dto/response/auth/index.ts
@@ -1,5 +1,5 @@
 import SignInResponseDto from './sign-in.response.dto';
 
 export type {
-    SignInResponseDto
+  SignInResponseDto
 }
--- src/apis/dto/response/auth/sign-in.response.dto.ts
+++ src/apis/dto/response/auth/sign-in.response.dto.ts
@@ -2,6 +2,6 @@ import { ResponseDto } from 'src/apis/dto/response';
 
 // interface: sign in response body DTO //
 export default interface SignInResponseDto extends ResponseDto {
-    accessToken: string;
-    expiration: number;
+  accessToken: string;
+  expiration: number;
 }
