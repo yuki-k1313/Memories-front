5aaeabeacaf4b80360dd597e98eb90856fec9737
yuki-k1313 <yuki1313@naver.com>
2025-03-26 13:45:19 +0900

    2025-03-21

--- src/stores/memory-test.store.ts
+++ src/stores/memory-test.store.ts
@@ -2,14 +2,14 @@ import { create } from "zustand";
 
 // interface: 기억력 검사 결과 상태 구조 //
 interface MemoryTestStore {
-	measurementTime: number;
-	setMeasurementTime: (measurementTime: number) => void;
+  measurementTime: number;
+  setMeasurementTime: (measurementTime: number) => void;
 }
 
 // function: 기억력 검사 스토어 생성 함수 //
 const useStore = create<MemoryTestStore>(set => ({
-	measurementTime: 0,
-	setMeasurementTime: (measurementTime: number) => set(state => ({ ...state, measurementTime })),
+  measurementTime: 0,
+  setMeasurementTime: (measurementTime: number) => set(state => ({ ...state, measurementTime })),
 }));
 
 export default useStore;
