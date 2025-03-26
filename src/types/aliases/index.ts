9536c2a05581bd3cf28b89149a18c384cde888a4
yuki-k1313 <yuki1313@naver.com>
2025-03-26 13:47:02 +0900

    2025-03-18

--- src/types/aliases/index.ts
+++ src/types/aliases/index.ts
@@ -1,11 +1,11 @@
+import Feeling from './feeling.alias';
+import Weather from './weather.alias';
 import AuthPage from './auth-page.alias';
 import JoinType from './join-type.alias';
-import Weather from './weather.alias';
-import Feeling from './feeling.alias';
 
 export type {
-    Feeling,
-    Weather,
-    AuthPage,
-    JoinType
+  Feeling,
+  Weather,
+  AuthPage,
+  JoinType
 }
