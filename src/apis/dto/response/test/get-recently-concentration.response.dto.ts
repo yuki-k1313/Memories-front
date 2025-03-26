c922c103ed991537234127213f7ca29ec6dce867
yuki-k1313 <yuki1313@naver.com>
2025-03-26 13:37:21 +0900

    2025-03-25

--- src/apis/dto/response/test/get-recently-concentration.response.dto.ts
+++ src/apis/dto/response/test/get-recently-concentration.response.dto.ts
@@ -3,6 +3,6 @@ import { ConcentrationTest } from 'src/types/interfaces';
 import ResponseDto from '../response.dto';
 
 // interface: get recently concentration resposne body DTO //
-export default interface GetRecentlyConcentrationResponseDto extends ResponseDto {
+export default interface GetRecentlyConcentrationResponseDto extends ResponseDto { 
   concentrationTests: ConcentrationTest[];
 }
