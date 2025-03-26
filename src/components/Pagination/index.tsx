6c897e88a6e0ce0ec70dc9b6511b27af5b8d162f
yuki-k1313 <yuki1313@naver.com>
2025-03-26 13:40:00 +0900

    2025-03-19

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -3,49 +3,50 @@ import './style.css';
 
 // interface: 페이지네이션 컴포넌트 속성 //
 interface Props {
-    currentPage: number;
-    currentSection: number;
-    totalSection: number;
-    pageList: number[];
-    setCurrentPage: React.Dispatch<React.SetStateAction<number>>;
-    setCurrentSection: React.Dispatch<React.SetStateAction<number>>;
+  currentPage: number;
+  currentSection: number;
+  totalSection: number;
+  pageList: number[];
+  setCurrentPage: React.Dispatch<React.SetStateAction<number>>;
+  setCurrentSection: React.Dispatch<React.SetStateAction<number>>;
 }
 
 // component: 페이지네이션 컴포넌트 //
-export default function Pagination({  
-    currentPage, currentSection, totalSection, pageList, setCurrentPage, setCurrentSection
+export default function Pagination({
+  currentPage, currentSection, totalSection, pageList,
+  setCurrentPage, setCurrentSection
 }: Props) {
 
-    // function: 페이지 클래스 //
-    const pageClass = (page: number) => currentPage === page ? 'page active' : 'page'; 
+  // function: 페이지 클래스 //
+  const pageClass = (page: number) => currentPage === page ? 'page active' : 'page';
 
-    // event handler: 페이지 변경 이벤트 처리 //
-    const onPageCLickHandler = (page: number) => {
-        setCurrentPage(page);
-    };
+  // event handler: 페이지 변경 이벤트 처리 //
+  const onPageClickHandler = (page: number) => {
+    setCurrentPage(page);
+  };
 
-    // event handler: 이전 섹션 클릭 이벤트 처리 //
-    const onPreSectionClickHandler = () => {
-        if(currentSection <= 1) return;
-        setCurrentSection(currentSection - 1);
-        setCurrentPage((currentSection - 1) * 10);
-    };
+  // event handler: 이전 섹션 클릭 이벤트 처리 //
+  const onPreSectionClickHandler = () => {
+    if (currentSection <= 1) return;
+    setCurrentSection(currentSection - 1);
+    setCurrentPage((currentSection - 1) * 10);
+  };
 
-    // event handler: 다음 섹션 클릭 이벤트 처리 //
-    const onNextSectioncClickHandler = () => {
-        if(currentSection === totalSection) return;
-        setCurrentSection(currentSection + 1);
-        setCurrentPage(currentSection * 10 + 1);
-    };
+  // event handler: 다음 섹션 클릭 이벤트 처리 //
+  const onNextSectionClickHandler = () => {
+    if (currentSection === totalSection) return;
+    setCurrentSection(currentSection + 1);
+    setCurrentPage(currentSection * 10 + 1);
+  };
 
-    // render: 페이지네이션 컴포넌트  렌더링//
-    return (
-        <div className='pagination-box'>
-            <div className='pagination-button left' onClick={onPreSectionClickHandler}></div>
-            <div className='pagination'>
-                {pageList.map((page, index) => <div key={index} className={pageClass(page)} onClick={() => onPageCLickHandler(page)}>{page}</div>)}
-            </div>
-            <div className='pagination-button right' onClick={onNextSectioncClickHandler}></div>
-        </div>        
-    )
+  // render: 페이지네이션 컴포넌트 렌더링 //
+  return (
+    <div className='pagination-box'>
+      <div className='pagination-button left' onClick={onPreSectionClickHandler}></div>
+      <div className='pagination'>
+        {pageList.map((page, index) => <div key={index} className={pageClass(page)} onClick={() => onPageClickHandler(page)}>{page}</div>)}
+      </div>
+      <div className='pagination-button right' onClick={onNextSectionClickHandler}></div>
+    </div>
+  )
 }
